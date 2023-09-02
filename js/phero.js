4db8acff58fe6c1f0a5dfcfeaeafa53f8a4f7ce2
cseDeveloper06 <faridayasmeencse@gmail.com>
2023-09-02 19:47:49 +0600

    added categories btn

--- js/phero.js
+++ js/phero.js
@@ -14,3 +14,24 @@ const handleHomeBtn = () =>{
 const handleBlogBtn = () =>{
     window.location.href = "blog.html";
 }
+
+const loadCategory = async()=>{
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await response.json();
+    console.log(data.data); 
+    const categoryContainer = document.getElementById('category-container');
+    data.data.forEach((category)=>{
+        //console.log(category)
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <button class="btn">${category.category}</button>
+        `;
+        categoryContainer.appendChild(div);
+    })
+}
+
+const loadAllVideos = () =>{
+    
+}
+
+loadCategory();
