bfd42e19c4b6abeb29ecb729b9874507466bac9b
cseDeveloper06 <faridayasmeencse@gmail.com>
2023-09-04 20:34:56 +0600

    added updates

--- js/phero.js
+++ js/phero.js
@@ -1,12 +1,4 @@
 
-const menuItems = document.querySelector('.toggle-menu-items');
-function onToggleMenu(e){
-    console.log(e);
-    e.name = e.name === 'menu' ? 'close' : 'menu';
-    //menuItems.classList.toggle('top-[25%]');
-    menuItems.classList.toggle('top-[25%]');
-}
-
 const handleHomeBtn = () =>{
     window.location.href = "index.html";
 }
