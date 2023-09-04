93401d7feb794ce56e7d0e4d923e0777a531d926
cseDeveloper06 <faridayasmeencse@gmail.com>
2023-09-04 20:27:58 +0600

    added updates

--- js/phero.js
+++ js/phero.js
@@ -18,7 +18,7 @@ const handleBlogBtn = () =>{
 const loadCategory = async()=>{
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await response.json();
-    //console.log(data.data); 
+    console.log(data.message); 
     const categoryContainer = document.getElementById('category-container');
     data.data.forEach((category)=>{
         //console.log(category)
@@ -28,15 +28,17 @@ const loadCategory = async()=>{
         `;
         categoryContainer.appendChild(div);
     })
+
 }
 
 const loadAllVideos = async(category_id) =>{
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`);
     const data = await response.json();
+    console.log(data.data);
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
-    //console.log(data.data);
-    data.data?.forEach((video)=>{
+
+    data.data.forEach((video)=>{ 
         const div = document.createElement('div');
         div.innerHTML = `
             <div class="card w-60 bg-base-100 shadow-xl rounded-xl">
@@ -44,18 +46,16 @@ const loadAllVideos = async(category_id) =>{
                     <img class="w-60 h-40 rounded-xl" src="${video?.thumbnail}" alt="" />
                 </a>
                 <div class="card-body">
-                    <div>
-                        <div>
+                    <div class="flex flex-col justify-start items-center">
+                        <div class="flex justify-start items-center gap-5">
                             <img class="h-10 w-10 rounded-full" src="${video.authors[0]?.profile_picture}" alt="">
                             <h2 class="card-title text-xs">${video.title}</h2>
                         </div> 
-                        <div>
-                            <h3 class="text-xs">${video.authors[0]?.profile_name}</h3>
-                            <i class="fa-solid fa-check text-white bg-blue-700 h-5 w-5 
-                            rounded-full text-xs font-bold text-center">${video.authors[0]?.verified ?video.authors[0].verified:" "}</i>
-                            
+                        <div class="flex justify-center items-center gap-3 ml-8">
+                            <h3 class="text-xs ml-4">${video.authors[0]?.profile_name}</h3>
+                            <span id="verified-id">${video.authors[0]?.verified? "v" : " "}</span>
                         </div>
-                        <p class="text-xs text-left">${video.others?.views} views</p>
+                        <p class="text-xs text-left ml-6">${video.others?.views} views</p>
                     </div>
                 </div>
             </div>
@@ -66,7 +66,7 @@ const loadAllVideos = async(category_id) =>{
 }
 
 const searchByView = ()=>{
-    
+
 }
 
 loadCategory();
